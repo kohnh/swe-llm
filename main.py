d94590a0d142e3db54250e002d36f39acd6de378
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2024-02-21 14:54:32 +0000

    Create base files

--- main.py
+++ main.py
@@ -22,17 +22,10 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
 @app.post("/conversations")
-def Creates_a_new_conversation_with_a_LLM_model(*convo: Convo):
+def Creates_a_new_conversation_with_a_LLM_model(convo: Convo | None = None):
     return {"name": "string", "params": {
+    "additionalProp1": {},
+    },
     "additionalProp1": {}
-  }}
+    }
