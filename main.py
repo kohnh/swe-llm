e9cc6671466d91675363a4c832947ec57a8aa32a
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2024-02-21 14:29:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+class Convo(BaseModel):
+    name: str
+    params: dict
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+@app.post("/conversations")
+def Creates_a_new_conversation_with_a_LLM_model(*convo: Convo):
+    return {"name": "string", "params": {
+    "additionalProp1": {}
+  }}
