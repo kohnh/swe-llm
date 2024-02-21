86538e77cd99000088194422177b0f2764106f8c
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2024-02-21 18:14:21 +0000

    Add schemas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from typing import Union
+from typing import Union, List, Optional
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from openai import OpenAI
 import os
@@ -11,6 +11,38 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import Document, Indexed, init_beanie
 from urllib.parse import quote_plus
 
+#Schemas
+class APIError(BaseModel):
+    code: int
+    message: str
+    request: Optional[dict]
+    details: Optional[dict]
+
+
+class Prompt(BaseModel):
+    role: str
+    content: str
+
+
+class Conversation(BaseModel):
+    id: str = Field(readonly=True)
+    name: str = Field(max_length=200)
+    params: Optional[dict]
+    tokens: Optional[int] = Field(ge=0, readonly=True)
+
+
+class ConversationFull(Conversation):
+    messages: List[Prompt]
+
+
+class ConversationPOST(BaseModel):
+    name: str = Field(max_length=200)
+    params: Optional[dict]
+
+
+class ConversationPUT(BaseModel):
+    name: str = Field(max_length=200)
+    params: Optional[dict]
 
 load_dotenv()
 openAI_client = OpenAI( api_key=os.environ.get("OPENAI_API_KEY"))
@@ -20,15 +52,13 @@ MODEL = "gpt-3.5-turbo"
 app = FastAPI()
 
 
-class Convo(BaseModel):
-    name: str
-    params: dict
+
 
 username = quote_plus(os.environ.get("USERNAME"))
 password = quote_plus(os.environ.get("PASSWORD"))
 uri = 'mongodb+srv://' + username + ':' + password + '@cluster0.duu8e7a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 beanie_client = AsyncIOMotorClient(uri)
-init_beanie(database=beanie_client.db_name, document_models=[Convo])
+init_beanie(database=beanie_client.db_name, document_models=[Conversation])
 
 
 @app.get("/")
@@ -36,16 +66,11 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/conversations")
-def Creates_a_new_conversation_with_a_LLM_model(convo: Convo | None = None):
+@app.post("/conversations")
+def new_conversation(convo: Conversation | None = None):
     response = openAI_client.chat.completions.create(
         model=MODEL,
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Knock knock."},
-            {"role": "assistant", "content": "Who's there?"},
-            {"role": "user", "content": "Orange."},
-        ],
+        messages=[],
         temperature=0,
     )
     return (response.choices[0].message.content)
